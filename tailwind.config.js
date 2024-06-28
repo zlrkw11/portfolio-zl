a7f3f1f36614741dc388dd758f3d52ed306215c4
ZL <ray040406@gmail.com>
2024-06-28 21:43:28 +1200

    font import

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [],
+  content: ["./index.html", "./src/**/*.{jsx,js}"],
+  darkMode: "class",
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        inter: ["inter", "serif"],
+      },
+    },
   },
   plugins: [],
-}
-
+};
