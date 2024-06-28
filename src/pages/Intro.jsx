fa304f80a661f9ec4cfe81649654a51f5a91540c
ZL <ray040406@gmail.com>
2024-06-29 11:44:42 +1200

    text animation component

--- src/pages/Intro.jsx
+++ src/pages/Intro.jsx
@@ -1,7 +1,15 @@
 function Intro() {
   return (
-    <div>
-      <h1 className="pl-4">ZL</h1>
+    <div className="font-extrabold text-3xl md:text-4xl [text-wrap:balance] bg-clip-text text-transparent bg-gradient-to-r from-slate-200/60 to-50% to-slate-200">
+      My Skills Are{" "}
+      <span className="text-indigo-500 inline-flex flex-col">
+        <ul className="block text-left leading-tight [&_li]:block">
+          <li>React</li>
+          <li>JavaScript</li>
+          <li>TypeScript</li>
+          <li>TailwindCSS</li>
+        </ul>
+      </span>
     </div>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        inter: ["inter", "serif"],
+      },
+    },
   },
   plugins: [],
 };
