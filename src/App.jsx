f11e0f7ec876d0c0a7a96cbdc91f8754857d0f65
ZL <ray040406@gmail.com>
2024-06-29 16:27:09 +1200

    links map fn

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,12 @@ import "./styles/tailwind.css";
 
 // pages
 import Main from "./pages/Main";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
     <div className="">
+      <Navbar />
       <Main />
     </div>
   );
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,21 @@
+function Navbar() {
+  let Links = [{ name: "HOME", link: "/" }];
+  return (
+    <div className="border border-white shadow-md w-full fixed top-0 left-0">
+      <div className="md:flex bg-white py-4">
+        <div className="font-bold text-2xl cursor-pointer flex items-center">
+          Navbar text
+        </div>
+        <ul>
+          {Links.map((link) => (
+            <li key={link.name}>
+              <a href={link.link}>{link.name}</a>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
+}
+
+export default Navbar;
