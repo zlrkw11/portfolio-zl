fb8f2ca180a4c8537275426bf330e00bdfa3c17e
ZL <ray040406@gmail.com>
2024-07-02 00:24:08 +1200

    expand svg rotate

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,8 +5,10 @@ import Linkedin from "../assets/icons/linkedin.svg?react";
 import UpArrow from "../assets/icons/up.svg?react";
 function Footer() {
   const [isExpanded, setIsExpanded] = useState(false);
+  const [isRotated, setIsRotated] = useState(false);
   function handleOnClick() {
     setIsExpanded(!isExpanded);
+    setIsRotated(!isRotated);
   }
   return (
     <div className="h-16 bottom-0 w-full fixed flex md:grid grid-cols-2 items-center justify-center bg-dark-0 gap-4">
@@ -32,7 +34,7 @@ function Footer() {
       <div
         className={`flex flex-col bottom-0 md:hidden transition-transform duration-300 ease-in-out  ${
           isExpanded ? "translate-y-28" : "-translate-y-12"
-        }`}
+        } ${isRotated ? "rotate-180" : ""}`}
       >
         <a href="https://github.com/zlrkw11" className="">
           <Github className="fill-white w-8 cursor-pointer icon" />
