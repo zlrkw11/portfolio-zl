a4562df400a41059d721999ab73a25e32e70a6c2
ZL <ray040406@gmail.com>
2024-07-02 00:01:49 +1200

    fixed footer, added animation

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -9,7 +9,7 @@ function Footer() {
     setIsExpanded(!isExpanded);
   }
   return (
-    <div className="h-16 flex md:grid grid-cols-2 items-center justify-center bg-dark-0 gap-4">
+    <div className="h-16 bottom-0 fixed flex md:grid grid-cols-2 items-center justify-center bg-dark-0 gap-4">
       <div className="pl-4 text-white">
         <p>&copy; {new Date().getFullYear()} zlrkw11. All rights reserved</p>
       </div>
@@ -18,6 +18,22 @@ function Footer() {
       </button>
       {/* svg icon links */}
       <div className="hidden md:flex gap-8 group items-center ml-auto mr-8">
+        <a href="https://github.com/zlrkw11" className="">
+          <Github className="fill-white w-8 cursor-pointer icon" />
+        </a>
+        <a href="https://www.linkedin.com/in/ray-zhao-1a2380269/">
+          <Linkedin className="fill-white w-8 cursor-pointer icon" />
+        </a>
+        <a href="https://www.instagram.com/zlrkw_11/">
+          <Insta className="fill-white w-8 cursor-pointer icon" />
+        </a>
+      </div>
+
+      <div
+        className={`flex flex-col bottom-0 md:hidden transition-transform duration-300 ease-in-out  ${
+          isExpanded ? "translate-y-28" : "-translate-y-12"
+        }`}
+      >
         <a href="https://github.com/zlrkw11" className="">
           <Github className="fill-white w-8 cursor-pointer icon" />
         </a>
