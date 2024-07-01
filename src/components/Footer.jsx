97219cf402aa806f726e6791a5d2d625f8dec275
ZL <ray040406@gmail.com>
2024-07-01 19:14:55 +1200

    z

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -8,9 +8,16 @@ function Footer() {
         <p>&copy; {new Date().getFullYear()} zlrkw11. All rights reserved</p>
       </div>
       {/* svg icon links */}
-      <div className="hidden md:flex gap-8 group ">
-        <Github className="fill-white w-8 cursor-pointer" />
-        <Linkedin className="fill-white w-8 cursor-pointer" />
+      <div className="hidden md:flex gap-8 group items-center ml-auto mr-8">
+        <a href="https://github.com/zlrkw11" className="">
+          <Github className="fill-white w-8 cursor-pointer " />
+        </a>
+        <a href="https://www.linkedin.com/in/ray-zhao-1a2380269/">
+          <Linkedin className="fill-white w-8 cursor-pointer" />
+        </a>
+        <a href="https://www.instagram.com/zlrkw_11/">
+          <Insta className="fill-white w-8 cursor-pointer" />
+        </a>
       </div>
     </div>
   );
