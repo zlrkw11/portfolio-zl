0018a6d71861c3e270a81405494098597865ca31
ZL <ray040406@gmail.com>
2024-07-01 17:47:15 +1200

    icon styling social media links

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,13 +1,16 @@
 import Github from "../assets/icons/github.svg?react";
+import Insta from "../assets/icons/insta.svg?react";
+import Linkedin from "../assets/icons/linkedin.svg?react";
 function Footer() {
   return (
-    <div className="h-16 grid grid-cols-2 items-center">
-      <div>
+    <div className="h-16 grid grid-col md:grid-cols-2 items-center justify-center bg-dark-0">
+      <div className="pl-4 text-white">
         <p>&copy; {new Date().getFullYear()} zlrkw11. All rights reserved</p>
       </div>
       {/* svg icon links */}
-      <div>
-        <Github />
+      <div className="hidden md:flex gap-8 group ">
+        <Github className="fill-white w-8 cursor-pointer" />
+        <Linkedin className="fill-white w-8 cursor-pointer" />
       </div>
     </div>
   );
