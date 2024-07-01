fb8f55fe3efca7caa6bd6037ec1a27657303ad5b
ZL <ray040406@gmail.com>
2024-07-01 19:50:29 +1200

    import gaming folder svg

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,12 +1,16 @@
 import Github from "../assets/icons/github.svg?react";
 import Insta from "../assets/icons/insta.svg?react";
 import Linkedin from "../assets/icons/linkedin.svg?react";
+import Folder from "../assets/icons/folder.svg?react";
 function Footer() {
   return (
-    <div className="h-16 grid grid-col md:grid-cols-2 items-center justify-center bg-dark-0">
+    <div className="h-16 flex md:grid grid-cols-2 items-center justify-center bg-dark-0">
       <div className="pl-4 text-white">
         <p>&copy; {new Date().getFullYear()} zlrkw11. All rights reserved</p>
       </div>
+      <button className="grid text-white md:hidden ">
+        <Folder className="fill-white border-white" />
+      </button>
       {/* svg icon links */}
       <div className="hidden md:flex gap-8 group items-center ml-auto mr-8">
         <a href="https://github.com/zlrkw11" className="">
