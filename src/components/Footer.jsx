f4f93c9dc8ddec9b46966b45f77521596cabc2bf
ZL <ray040406@gmail.com>
2024-07-01 17:25:26 +1200

    footer imports

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,14 @@
+import Github from "../assets/icons/github.svg?react";
 function Footer() {
   return (
-    <div className="h-8 grid grid-cols-2 items-center">
-      <p>&copy; {new Date().getFullYear()} zlrkw11. All rights reserved</p>
+    <div className="h-16 grid grid-cols-2 items-center">
+      <div>
+        <p>&copy; {new Date().getFullYear()} zlrkw11. All rights reserved</p>
+      </div>
+      {/* svg icon links */}
+      <div>
+        <Github />
+      </div>
     </div>
   );
 }
--- src/components/GitHubTag.jsx
+++
@@ -1,7 +0,0 @@
-import Logo from "../assets/icons/github-mark-white.svg?react";
-
-function GitHubTag() {
-  return <div className=""></div>;
-}
-
-export default GitHubTag;
