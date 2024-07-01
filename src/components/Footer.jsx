195dafc2ad37d10af6f0fb36f684a54ce90b88bb
ZL <ray040406@gmail.com>
2024-07-01 20:07:48 +1200

    expand handle function

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,14 +1,19 @@
+import { useState } from "react";
 import Github from "../assets/icons/github.svg?react";
 import Insta from "../assets/icons/insta.svg?react";
 import Linkedin from "../assets/icons/linkedin.svg?react";
 import UpArrow from "../assets/icons/up.svg?react";
 function Footer() {
+  const [isExpanded, setIsExpanded] = useState(false);
+  function handleOnClick() {
+    setIsExpanded(!isExpanded);
+  }
   return (
     <div className="h-16 flex md:grid grid-cols-2 items-center justify-center bg-dark-0 gap-4">
       <div className="pl-4 text-white">
         <p>&copy; {new Date().getFullYear()} zlrkw11. All rights reserved</p>
       </div>
-      <button className="grid text-white md:hidden">
+      <button className="grid text-white md:hidden" onClick={handleOnClick}>
         <UpArrow className="fill-white icon" />
       </button>
       {/* svg icon links */}
