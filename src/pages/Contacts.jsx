317d75619e14c9bac907170c6f9449c14be4be7e
ZL <ray040406@gmail.com>
2024-07-02 20:32:06 +1200

    title styling

--- src/pages/Contacts.jsx
+++ src/pages/Contacts.jsx
@@ -2,7 +2,7 @@ function Contacts() {
   return (
     <div className="w-full min-h-screen bg-dark-0 grid grid-rows-2">
       <div className="mt-8">
-        <h1>CONTACT ME</h1>
+        <h1 className="text-white text-3xl">CONTACT ME</h1>
       </div>
     </div>
   );
