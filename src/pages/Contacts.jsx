a91729822a283a6572fd4982092f389933bb8da3
ZL <ray040406@gmail.com>
2024-07-03 01:01:30 +1200

    position adjusted for title

--- src/pages/Contacts.jsx
+++ src/pages/Contacts.jsx
@@ -1,19 +1,16 @@
 import { motion } from "framer-motion";
 function Contacts() {
   return (
-    <div className="w-full min-h-screen bg-dark-0 grid grid-rows-2 title">
-      <motion.h2
-        className="cursor-grab "
-        drag
-        dragConstraints={{ top: -125, bottom: 75, left: 200, right: 200 }}
-        initial={{ opacity: 0, y: 330 }}
+    <div className="w-full min-h-screen bg-dark-0 grid grid-col title items-center justify-center">
+      <motion.div
+        initial={{ opacity: 0, y: 300 }}
         animate={{ opacity: 1, y: 0 }}
         transition={{ duration: 1, ease: "easeInOut", delay: 0.2 }}
       >
-        <div className="mt-8">
+        <div className="mt-8 items-center flex">
           <h1 className="text-white text-3xl font-bold">CONTACT ME</h1>
         </div>
-      </motion.h2>
+      </motion.div>
     </div>
   );
 }
