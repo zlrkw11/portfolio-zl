643d844a7d2a1f7e531d11062528d18e5f627e60
ZL <ray040406@gmail.com>
2024-07-03 00:38:24 +1200

    dragging on text

--- src/pages/Contacts.jsx
+++ src/pages/Contacts.jsx
@@ -2,11 +2,14 @@ import { motion } from "framer-motion";
 function Contacts() {
   return (
     <div className="w-full min-h-screen bg-dark-0 grid grid-rows-2">
-      <div className="mt-8">
-        <h1 className="text-white text-3xl font-bold">CONTACT ME</h1>
-      </div>
-      <motion.h2 className="cursor-grab " drag>
-        Contact Me
+      <motion.h2
+        className="cursor-grab "
+        drag
+        dragConstraints={{ top: -125, bottom: 0, left: 200, right: 200 }}
+      >
+        <div className="mt-8">
+          <h1 className="text-white text-3xl font-bold">CONTACT ME</h1>
+        </div>
       </motion.h2>
     </div>
   );
