0ab1dacd08c9fe40049ff275953abfa9cd4e5dfe
ZL <ray040406@gmail.com>
2024-07-03 00:33:25 +1200

    setup framer motion

--- src/pages/Contacts.jsx
+++ src/pages/Contacts.jsx
@@ -1,9 +1,11 @@
+import { motion } from "framer-motion";
 function Contacts() {
   return (
     <div className="w-full min-h-screen bg-dark-0 grid grid-rows-2">
       <div className="mt-8">
-        <h1 className="text-white text-3xl">CONTACT ME</h1>
+        <h1 className="text-white text-3xl font-bold">CONTACT ME</h1>
       </div>
+      <motion.h2 animate={{ fontSize: 50 }}>Contact Me</motion.h2>
     </div>
   );
 }
