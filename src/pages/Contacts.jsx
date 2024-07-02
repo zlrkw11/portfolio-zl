f5aae25d0a0a38093a9c374443df47177845e362
ZL <ray040406@gmail.com>
2024-07-03 01:04:28 +1200

    position

--- src/pages/Contacts.jsx
+++ src/pages/Contacts.jsx
@@ -1,13 +1,13 @@
 import { motion } from "framer-motion";
 function Contacts() {
   return (
-    <div className="w-full min-h-screen bg-dark-0 grid grid-col title items-center justify-center">
+    <div className="w-full min-h-screen bg-dark-0 grid grid-col title justify-center">
       <motion.div
-        initial={{ opacity: 0, y: 300 }}
+        initial={{ opacity: 0, y: 250 }}
         animate={{ opacity: 1, y: 0 }}
         transition={{ duration: 1, ease: "easeInOut", delay: 0.2 }}
       >
-        <div className="mt-8 items-center flex">
+        <div className="mt-8">
           <h1 className="text-white text-3xl font-bold">CONTACT ME</h1>
         </div>
       </motion.div>
