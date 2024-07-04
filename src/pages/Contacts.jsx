a9841856f77f6f8f9fe6e32ea6d7b60bfcefb9e3
ZL <ray040406@gmail.com>
2024-07-05 00:24:35 +1200

    hover border

--- src/pages/Contacts.jsx
+++ src/pages/Contacts.jsx
@@ -2,20 +2,26 @@ import { motion } from "framer-motion";
 
 function Contacts() {
   return (
-    <div className="w-full h-screen bg-dark-0 grid grid-col  justify-center">
+    <div className="w-full h-screen bg-dark-0 flex flex-col justify-center items-center">
       {/* <motion.div
         initial={{ opacity: 0, y: 250 }}
         animate={{ opacity: 1, y: 0 }}
         transition={{ duration: 1, ease: "easeInOut", delay: 0.2 }}
       ></motion.div> */}
-      <div className="mt-36">
+      <div className="">
         <h1 className="text-white text-3xl font-bold title">CONTACT ME</h1>
       </div>
 
-      <div className="flex flex-col gap-4 group">
-        <input placeholder="Name"></input>
-        <input placeholder="Subject"></input>
-        <input placeholder="Message"></input>
+      <div className="flex flex-col gap-4 items-center justify-center mt-4">
+        <label className="relative" />
+        <input
+          required
+          type="text"
+          placeholder="Name"
+          className="px-4 py-2 text-lg outline-none border-2 border-gray-700 rounded hover:border-white duration-200 peer focus:border-white bg-inherit"
+        ></input>
+
+        <label />
       </div>
     </div>
   );
