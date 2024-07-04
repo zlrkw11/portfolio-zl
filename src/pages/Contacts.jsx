bf8ebcf26db552ccd5f6fe53026c777e44cd6d39
ZL <ray040406@gmail.com>
2024-07-05 00:48:09 +1200

    send button

--- src/pages/Contacts.jsx
+++ src/pages/Contacts.jsx
@@ -12,27 +12,28 @@ function Contacts() {
         <h1 className="text-white text-3xl font-bold title">CONTACT ME</h1>
       </div>
 
-      <div className="flex flex-col gap-4 items-center justify-center mt-4">
-        <label className="relative" />
+      <div className="flex flex-col gap-4 items-center justify-center mt-8">
         <input
           required
           type="text"
           placeholder="Name"
-          className="px-4 py-2 text-lg box text-white outline-none border-2 border-dark-2 rounded hover:border-white duration-200 peer focus:border-white bg-inherit hover:-translate-x-4"
+          className="px-4 py-2 text-lg box text-white outline-none border-2 border-dark-2 rounded hover:border-white duration-200 peer focus:border-white bg-inherit hover:-translate-x-4 transition"
         ></input>
         <input
           required
           type="text"
           placeholder="Subject"
-          className="px-4 py-2 text-lg box text-white outline-none border-2 border-dark-2 rounded hover:border-white duration-200 peer focus:border-white bg-inherit hover:translate-x-4"
+          className="px-4 py-2 text-lg box text-white outline-none border-2 border-dark-2 rounded hover:border-white duration-200 peer focus:border-white bg-inherit hover:translate-x-4 transition"
         ></input>
         <input
           required
           type="text"
-          placeholder="Subject"
-          className="px-4 py-6 text-lg box text-white outline-none border-2 border-dark-2 rounded hover:border-white duration-200 peer focus:border-white bg-inherit hover:translate-y-4"
+          placeholder="Message For Z.L"
+          className="px-4 py-6 text-lg box text-white outline-none border-2 border-dark-2 rounded hover:border-white duration-200 peer focus:border-white bg-inherit hover:-translate-x-4 transition"
         ></input>
-        <label />
+        <button className="px-4 py-2 rounded border-dark-2 border-2 text-dark-2 box hover:border-white hover:text-white outline-none focus:border-white">
+          Send
+        </button>
       </div>
     </div>
   );
