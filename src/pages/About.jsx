8efb89c85121581fce309074332e71c56928c0be
ZL <ray040406@gmail.com>
2024-07-07 00:51:44 +1200

    import useRef

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,8 +1,13 @@
 import { motion } from "framer-motion";
 import { useScroll } from "framer-motion";
+import { useRef } from "react";
+
 function About() {
+  const ref = useRef(null);
+
   useScroll({
     target: "",
+    offset: ["0 1", "1.33 1"],
   });
   return <div></div>;
 }
