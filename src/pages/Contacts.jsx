518bf479a681cd2e6e7b1f12031dafde03deaaaf
ZL <ray040406@gmail.com>
2024-07-06 12:44:23 +1200

    key for message sent

--- src/pages/Contacts.jsx
+++ src/pages/Contacts.jsx
@@ -40,6 +40,7 @@ function Contacts() {
           <input
             required
             type="text"
+            name="message"
             placeholder="Message For Z.L"
             className="px-4 py-6 text-lg box text-white outline-none border-2 border-dark-2 rounded hover:border-white duration-200 peer focus:border-white bg-inherit hover:-translate-x-4 transition"
           ></input>
