fbd47cd9e7d1bdc65a4176ca07546bf9df4429ed
ZL <ray040406@gmail.com>
2024-07-06 12:34:33 +1200

    form styling move

--- src/pages/Contacts.jsx
+++ src/pages/Contacts.jsx
@@ -10,8 +10,8 @@ function Contacts() {
         <h1 className="text-white text-3xl font-bold title">CONTACT ME</h1>
       </div>
 
-      <div className="flex flex-col gap-4 items-center justify-center mt-8">
-        <form>
+      <div>
+        <form className="flex flex-col gap-4 items-center justify-center mt-8">
           <input
             required
             type="text"
@@ -37,27 +37,6 @@ function Contacts() {
             Send
           </button>
         </form>
-        <input
-          required
-          type="text"
-          placeholder="Name"
-          className="px-4 py-2 text-lg box text-white outline-none border-2 border-dark-2 rounded hover:border-white ease-in-out duration-200 peer focus:border-white bg-inherit hover:-translate-x-4 transition"
-        ></input>
-        <input
-          required
-          type="text"
-          placeholder="Subject"
-          className="px-4 py-2 text-lg box text-white outline-none border-2 border-dark-2 rounded hover:border-white ease-in-out duration-200 peer focus:border-white bg-inherit hover:translate-x-4 transition"
-        ></input>
-        <input
-          required
-          type="text"
-          placeholder="Message For Z.L"
-          className="px-4 py-6 text-lg box text-white outline-none border-2 border-dark-2 rounded hover:border-white ease-in-out duration-200 peer focus:border-white bg-inherit hover:-translate-x-4 transition"
-        ></input>
-        <button className="px-4 py-2 rounded border-dark-2 border-2 text-dark-2 box hover:border-white hover:text-white outline-none focus:border-white">
-          Send
-        </button>
       </div>
     </div>
   );
