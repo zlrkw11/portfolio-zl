223ef6a91a06169a54e9ec5a1992c3de1587b303
ZL <ray040406@gmail.com>
2024-07-06 12:32:23 +1200

    wrapped around form tags

--- src/pages/Contacts.jsx
+++ src/pages/Contacts.jsx
@@ -11,27 +11,32 @@ function Contacts() {
       </div>
 
       <div className="flex flex-col gap-4 items-center justify-center mt-8">
-        <input
-          required
-          type="text"
-          placeholder="Name"
-          className="px-4 py-2 text-lg box text-white outline-none border-2 border-dark-2 rounded hover:border-white duration-200 peer focus:border-white bg-inherit hover:-translate-x-4 transition"
-        ></input>
-        <input
-          required
-          type="text"
-          placeholder="Subject"
-          className="px-4 py-2 text-lg box text-white outline-none border-2 border-dark-2 rounded hover:border-white duration-200 peer focus:border-white bg-inherit hover:translate-x-4 transition"
-        ></input>
-        <input
-          required
-          type="text"
-          placeholder="Message For Z.L"
-          className="px-4 py-6 text-lg box text-white outline-none border-2 border-dark-2 rounded hover:border-white duration-200 peer focus:border-white bg-inherit hover:-translate-x-4 transition"
-        ></input>
-        <button className="px-4 py-2 rounded border-dark-2 border-2 text-dark-2 box hover:border-white hover:text-white outline-none focus:border-white">
-          Send
-        </button>
+        <form>
+          <input
+            required
+            type="text"
+            placeholder="Name"
+            className="px-4 py-2 text-lg box text-white outline-none border-2 border-dark-2 rounded hover:border-white duration-200 peer focus:border-white bg-inherit hover:-translate-x-4 transition"
+          ></input>
+          <input
+            required
+            type="text"
+            placeholder="Subject"
+            className="px-4 py-2 text-lg box text-white outline-none border-2 border-dark-2 rounded hover:border-white duration-200 peer focus:border-white bg-inherit hover:translate-x-4 transition"
+          ></input>
+          <input
+            required
+            type="text"
+            placeholder="Message For Z.L"
+            className="px-4 py-6 text-lg box text-white outline-none border-2 border-dark-2 rounded hover:border-white duration-200 peer focus:border-white bg-inherit hover:-translate-x-4 transition"
+          ></input>
+          <button
+            type="submit"
+            className="px-4 py-2 rounded border-dark-2 border-2 text-dark-2 box hover:border-white hover:text-white outline-none focus:border-white"
+          >
+            Send
+          </button>
+        </form>
       </div>
     </div>
   );
