f28e851d6ea8c7d732bb3522fbdefd856befe7c3
ZL <ray040406@gmail.com>
2024-07-07 00:54:34 +1200

    style

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -3,13 +3,18 @@ import { useScroll } from "framer-motion";
 import { useRef } from "react";
 
 function About() {
-  const ref = useRef(null);
+  const ref = useRef < HTMLElement > null;
 
   useScroll({
-    target: "",
+    target: ref,
     offset: ["0 1", "1.33 1"],
   });
-  return <div></div>;
+  return (
+    <motion.div
+      ref={ref}
+      style={{ scale: scrollY, opacity: scrollY }}
+    ></motion.div>
+  );
 }
 
 export default About;
