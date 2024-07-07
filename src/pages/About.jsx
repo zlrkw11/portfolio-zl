13a80c30ae299a3401d26d343bd51aaaae5a9102
ZL <ray040406@gmail.com>
2024-07-08 11:01:46 +1200

    added about text & styling

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,13 +1,25 @@
 import { motion, useScroll, useTransform } from "framer-motion";
 
 function About() {
-  const { scrollY } = useScroll();
-  const y = useTransform(scrollY, [0, 300], [0, 100]);
+  const { scrollYProgress } = useScroll();
+  const opacity = useTransform(scrollYProgress, [0.3, 0.7], [1, 0]);
   return (
     <div className="w-full h-screen bg-dark-0 flex flex-col justify-center items-center">
-      <div>
-        <motion.div className="text-white text-4xl" style={{ y }}>
+      <div className=" flex-col flex gap-4 h-8 w-[800px]">
+        <motion.div
+          className="text-center text-white text-5xl font-bold"
+          style={{ opacity }}
+        >
           About
+        </motion.div>
+        <motion.div className="text-white text-xl" style={{ opacity }}>
+          A deeply dedicated and enthusiastic second-year computer science
+          student at the University of Auckland with a strong focus on front-end
+          web development. Extremely passionate about web development and app
+          making, with proven expertise in independent project building. Seeking
+          an internship role to leverage my skills in React.js, JavaScript, and
+          TypeScript while contributing to team projects and gaining
+          professional experience.
         </motion.div>
       </div>
     </div>
