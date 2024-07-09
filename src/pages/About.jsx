824565e9243d4221cd74e02a385b9a6f399cd8ec
ZL <ray040406@gmail.com>
2024-07-09 22:44:58 +1200

    hover transitions

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -2,7 +2,7 @@ import { motion, useScroll, useTransform } from "framer-motion";
 
 function About() {
   const { scrollYProgress } = useScroll();
-  const opacity = useTransform(scrollYProgress, [0.3, 0.7], [1, 0]);
+  const opacity = useTransform(scrollYProgress, [0.5, 0.7], [1, 0]);
   return (
     <div className="w-full h-screen bg-dark-0 flex flex-col justify-center items-center">
       <div className=" flex-col flex gap-8 m-8 h-8 w-auto items-center justify-center">
@@ -13,17 +13,22 @@ function About() {
           About
         </motion.div>
         <motion.div
-          className="text-white text-xl md:w-[800px]"
+          className="text-white text-xl md:w-[800px]  "
           style={{ opacity }}
         >
-          A deeply <span className="title text-3xl">dedicated</span> and{" "}
-          <span className="title text-3xl">enthusiastic</span> second-year
+          A deeply <span className="title text-2xl">dedicated</span> and{" "}
+          <span className="title text-2xl">enthusiastic</span> second-year
           computer science student at the University of Auckland with a strong
-          focus on front-end web development. Extremely passionate about web
-          development and app making, with proven expertise in independent
-          project building. Seeking an internship role to leverage my skills in
-          React.js, JavaScript, and TypeScript while contributing to team
-          projects and gaining professional experience.
+          focus on front-end web development.
+          <div className="h-8 "></div>
+          <div className="hover:translate-x-16 duration-300 transition">
+            Extremely passionate about web development and app making, with
+            proven expertise in independent project building. Seeking an
+            internship role to leverage my skills in React.js, JavaScript, and
+            TypeScript while contributing to team projects and gaining
+            professional experience as I focus heavily on both team-building and
+            personal growth.
+          </div>
         </motion.div>
       </div>
     </div>
