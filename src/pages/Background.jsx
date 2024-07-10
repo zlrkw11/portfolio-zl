309ffd8f5d9d461680293942b9170c0dd3ba5ddb
ZL <ray040406@gmail.com>
2024-07-11 00:39:58 +1200

    comment

--- src/pages/Background.jsx
+++ src/pages/Background.jsx
@@ -1,11 +1,11 @@
-const Background = ({ children }) => {
-  return (
-    <div className="w-full h-full min-h-screen flex items-center justify-center bg-dark-0 overflow-hidden ">
-      <div className=" absolute inset-0 bg-gradient-to-r from-dark-0 to-dark-2 animated-blur"></div>
+// const Background = ({ children }) => {
+//   return (
+//     <div className="w-full h-full min-h-screen flex items-center justify-center bg-dark-0 overflow-hidden ">
+//       <div className=" absolute inset-0 bg-gradient-to-r from-dark-0 to-dark-2 animated-blur"></div>
 
-      <div className="relative z-10">{children}</div>
-    </div>
-  );
-};
+//       <div className="relative z-10">{children}</div>
+//     </div>
+//   );
+// };
 
-export default Background;
+// export default Background;
