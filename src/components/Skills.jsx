b44882ceb066b002964cb08944ecaee9f2921fb9
ZL <ray040406@gmail.com>
2024-07-11 00:53:14 +1200

    light effect

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,7 +1,7 @@
 function Skills() {
   return (
     <div className=" font-bold text-lg md:text-4xl [text-wrap:balance] bg-clip-text text-transparent gap-4   to-30% to-slate-100 flex">
-      <div className="text-white "> I develop with </div>
+      <div className="text-white title"> I develop with </div>
 
       <div className="text-white inline-flex flex-col h-[calc(theme(fontSize.xl)*theme(lineHeight.tight))] md:h-[calc(theme(fontSize.4xl)*theme(lineHeight.tight))] overflow-hidden">
         <div className="animate-text-slide-4 text-left leading-tight">
