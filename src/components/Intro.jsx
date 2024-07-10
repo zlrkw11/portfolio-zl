f64e7dcb7ad15b74eeeacd27afa433bd3a763c76
ZL <ray040406@gmail.com>
2024-07-11 00:23:15 +1200

    bg

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -1,7 +1,7 @@
 function Intro() {
   return (
     <div className="flex items-center justify-center p-10 h-auto ">
-      <div className="w-max">
+      <div>
         <h3 className=" animate-typing overflow-hidden whitespace-nowrap pr-5 text-5xl text-white font-bold">
           Hello, I am Ray.
         </h3>
