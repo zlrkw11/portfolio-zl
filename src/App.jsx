2bc37cb0bf69e2c05d2cec058143937516714888
ZL <ray040406@gmail.com>
2024-07-11 19:18:32 +1200

    route set

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import Main from "./pages/Main";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 import Contacts from "./pages/Contacts";
+import ProjectSection from "./pages/Projects";
 import About from "./pages/About";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 
@@ -16,6 +17,7 @@ function App() {
         <Route path="/" element={<Main />} />
         <Route path="/about" element={<About />} />
         <Route path="/contacts" element={<Contacts />} />
+        <Route path="/projects" element={<ProjectSection />} />
       </Routes>
       <Footer />
     </BrowserRouter>
