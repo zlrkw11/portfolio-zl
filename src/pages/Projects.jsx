45b0ec67db995c1e62fd95fc2e1ee7dfd439760f
ZL <ray040406@gmail.com>
2024-09-18 20:58:10 +1200

    loaded

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -35,15 +35,11 @@ const ProjectSection = () => {
           Booking System. Established in 2023.
         </div>
       </div>
-      <div className="border border-black w-[400px] h-[200px] flex items-center">
-        <div className="w-[30%] border border-white h-auto m-2">
-          image section
-        </div>
-        <div className="w-[70%] border border-white h-auto p-2 m-2">
-          Project with the University of Auckland Snowsports Club Website &
-          Booking System. Established in 2023.
-        </div>
-      </div>
+
+      <Project
+        image="https://example.com/image.jpg"
+        text="Project with the University of Auckland Snowsports Club Website & Booking System. Established in 2023."
+      />
     </div>
   );
 };
