2e7cd908b04f2fa53211c40f91ba8661d04aedc8
ZL <ray040406@gmail.com>
2024-09-18 20:43:33 +1200

    borders set for testing

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -2,7 +2,10 @@ const ProjectSection = () => {
   return (
     <div className="w-full h-screen bg-dark-0 flex flex-col justify-center items-center gap-4">
       <h1 className="text-white text-3xl">Projects</h1>
-      <div className="border border-black w-full h-[200px]"></div>
+      <div className="border border-black w-[400px] h-[200px] flex items-center">
+        <div className="w-[30%] border border-red">image section</div>
+        <div className="w-[70%] border border-red">description</div>
+      </div>
     </div>
   );
 };
