07eb10f1ba3d58bf44db9db7da26edcd124b9b83
ZL <ray040406@gmail.com>
2024-10-07 00:35:39 +1300

    photo

--- src/components/MusicPlayer.jsx
+++ src/components/MusicPlayer.jsx
@@ -1,14 +1,22 @@
 import { useState } from "react";
 import song from "../assets/secret_sign.flac";
+import album from "../assets/album.jpg";
 const MusicPlayer = () => {
   const [isPlaying, setisPlaying] = useState(false);
 
   function play() {
-    new Audio(song);
+    new Audio(song).play();
   }
+
   return (
-    <div className="">
-      <button onClick={play}>play</button>
+    <div className="border p-4 flex">
+      <img src={album} className="w-16" />
+      <button
+        className="text-slate-500 hover:text-white duration-500 font-bold p-2"
+        onClick={play}
+      >
+        play
+      </button>
     </div>
   );
 };
