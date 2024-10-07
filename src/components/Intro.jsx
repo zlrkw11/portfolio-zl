8256fb622501ff8396c4c0c7c19a57ae3e616e40
ZL <ray040406@gmail.com>
2024-10-07 22:51:53 +1300

    removed border for title

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -2,7 +2,7 @@ function Intro() {
   return (
     <div className="flex items-center justify-center h-auto ">
       <div>
-        <h4 className="hover:border-b-4 animate-typing overflow-hidden whitespace-nowrap mb-4 text-5xl text-white font-bold">
+        <h4 className="animate-typing overflow-hidden whitespace-nowrap pb-10 text-5xl text-white font-bold">
           Hello, I am Ray.
         </h4>
       </div>
