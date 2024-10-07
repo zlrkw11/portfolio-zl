25dbd9bb08c52af2413a0061b21a357264cfd332
ZL <ray040406@gmail.com>
2024-10-07 23:22:51 +1300

    fixing title spacing

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -3,20 +3,24 @@ function About() {
   return (
     <div className="min-w-screen min-h-[140dvh] justify-center overflow-y-auto bg-dark-0 flex flex-col">
       <div className=" flex-col flex gap-8 m-8 h-8 w-auto items-center justify-center">
-        <div className="text-white text-5xl font-bold mr-auto title">About</div>
-        <div className="text-white text-xl md:w-[800px]">
-          A deeply <span className="title text-2xl">dedicated</span> and{" "}
-          <span className="title text-2xl">enthusiastic</span> second-year
-          computer science student at the University of Auckland with a strong
-          focus on front-end web development.
-          <div className="h-8 "></div>
-          <div className="">
-            Extremely passionate about web development and app making, with
-            experience in independent project building. Seeking an internship
-            role to leverage my skills in React.js, JavaScript, and TypeScript
-            while contributing to team projects and gaining professional
-            experience as I focus heavily on both team-building and independent
-            personal growth.
+        <div>
+          <div className="text-white text-5xl font-bold mr-auto mb-4 title">
+            About
+          </div>
+          <div className="text-white text-xl md:w-[800px]">
+            A deeply <span className="title text-2xl">dedicated</span> and{" "}
+            <span className="title text-2xl">enthusiastic</span> second-year
+            computer science student at the University of Auckland with a strong
+            focus on front-end web development.
+            <div className="h-8 "></div>
+            <div className="">
+              Extremely passionate about web development and app making, with
+              experience in independent project building. Seeking an internship
+              role to leverage my skills in React.js, JavaScript, and TypeScript
+              while contributing to team projects and gaining professional
+              experience as I focus heavily on both team-building and
+              independent personal growth.
+            </div>
           </div>
         </div>
         <div className="text-white">
